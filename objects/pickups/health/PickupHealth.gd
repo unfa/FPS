1e0eb161175110b84e0662ca9b8e14440f5ed6a5
Kasia <kasia@kasia.home>
2018-08-25 00:04:44 +0200

    Initiated a separate PickupHealth class

---
+++ objects/pickups/health/PickupHealth.gd
@@ -0,0 +1,15 @@
+extends "res://objects/pickups/Pickup.gd"
+
+export var hp = 25
+
+var contents = {"health" : hp}
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
