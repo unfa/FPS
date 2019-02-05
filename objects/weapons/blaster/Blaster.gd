1f6970a9cb9cf9304fb6f76ac6b6f4b7677ba897
unfa <unfa00@gmail.com>
2019-02-06 00:35:12 +0100

    Blaster gun consumes ammo and won't fire unless it's present

--- objects/weapons/blaster/Blaster.gd
+++ objects/weapons/blaster/Blaster.gd
@@ -10,8 +10,11 @@ func _ready():
 	pass
 	
 func _input(event):
-	if Input.is_action_just_pressed("weapon_fire_primary"):
-		$AnimationPlayer.play("FirePrimary")
+	if Input.is_action_just_pressed("weapon_fire_primary"): # check if the primary fire action was just pressed
+		if $"../../../..".inventory.has("ammo_blaster"): # check if we have the proper ammo in inventory
+			if $"../../../..".inventory["ammo_blaster"] > 0: #check if the amount is higher than zero
+				$AnimationPlayer.play("FirePrimary") # run the animation
+				$"../../../..".inventory["ammo_blaster"] -= 1 # consume ammo
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
