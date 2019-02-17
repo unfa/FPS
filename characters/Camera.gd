2651c5f9f83535c23ff4430135ebb5ae9a3932eb
unfa <unfa00@gmail.com>
2019-02-17 01:59:18 +0100

    Trying again

---
+++ characters/Camera.gd
@@ -0,0 +1,17 @@
+extends Camera
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	get_tree().set("currentCamera", self)
+	
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
